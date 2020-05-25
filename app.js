f09313266237a9d9aed5227f8031889f4732aa01
Jash <jsotoh@hotmail.es>
2020-05-25 10:30:08 -0600

    primer commit
    Signed-off-by: "Jash" <"jsotoh@hotmail.es">

---
+++ app.js
@@ -0,0 +1,140 @@
+	
+	if ( navigator.serviceWorker ) {
+    	navigator.serviceWorker.register('sw.js');
+	}
+
+	function cargarPagina(){
+		var divReloj=document.getElementById("reloj");
+		divReloj.style.display='none';	
+	}
+	
+	function comenzar(){
+
+		var divOcultar=document.getElementById('datos');
+		var divReloj=document.getElementById("reloj");
+		var divReloj2=document.getElementById("reloj2");
+		var txtSerie=document.getElementById('serie');
+		var txtTiempo=document.getElementById('tiempo');
+		var txtDescanso=document.getElementById('descanso');
+		var lblSegundos=document.getElementById('lblSegundos');
+
+		divOcultar.style.display='none';
+		
+		divReloj.style.display='block';
+		divReloj2.style.display='block';
+		var totalTiempo=txtTiempo.value;
+		var serie=txtSerie.value;
+		var descanso=txtDescanso.value;
+
+		
+			var snd1 = new Audio("coche_8.mp3");
+  			snd1.play();	
+	
+		
+		
+
+			var timeleft = totalTiempo;
+			var bandera=true;
+			var contador=0;
+			var downloadTimer = setInterval(function(){
+
+				
+     			document.getElementById("lblSegundos").innerHTML =  timeleft--;
+     			//console.log("timeleft",timeleft);
+     			//console.log("serie%",serie);
+     			//console.log("descanso",descanso);
+     			if(timeleft <= 0 && serie>0){
+       				console.log("bandera",bandera);
+       				if (bandera){
+       					timeleft=descanso;
+       					bandera=false;
+       					serie--;
+       					console.log("bandera==0");
+
+       					var snd = new Audio("coche_8.mp3");
+  						snd.play();
+  						document.getElementById('tituloTiempo').innerHTML="DESCANSO";
+  						document.body.style.backgroundColor = "White";
+       				}else{
+       					timeleft=totalTiempo;
+       					bandera=true;
+       					console.log	("bandera==1");
+       					document.getElementById('tituloTiempo').innerHTML="ENTRENAR";
+       					var snd = new Audio("coche_8.mp3");
+  						snd.play();
+  						document.body.style.backgroundColor = "#BDB76B";
+       				}
+       				if(serie==0){
+       					clearInterval(downloadTimer);
+       					divOcultar.style.display='block';
+		
+						divReloj.style.display='none';
+						divReloj2.style.display='none';
+						location.reload();
+       				}
+       				/*contador++;
+       				
+       				if(contador%2==0){
+       					bandera=0;
+       					timeleft=descanso;
+       					serie--;
+       				}else{
+       					
+       					bandera=1;
+       					timeleft=totalTiempo;
+       				}
+
+       				//if (bandera==0){
+       					       				//}
+
+
+       				
+       				console.log("serie",serie);
+       				console.log	("intervalo",timeleft);
+       				console.log	('contador',contador);
+       				*/
+
+     			}
+			},1000);
+			
+	
+		
+		
+
+
+
+	}
+
+var totalTiempo=10;
+	function actualizarReloj(){
+		
+		var lblSegundos=document.getElementById('lblSegundos');
+		/*if(totalTiempo==0){
+			alert('Final');
+		}else{
+			lblSegundos.innerHTML=totalTiempo;
+			totalTiempo-=1;
+			setTimeout(actualizarReloj(),1000);
+			
+		}
+		*/
+		console.log(totalTiempo)
+		lblSegundos.innerHTML=totalTiempo
+		totalTiempo-=1;
+		if(totalTiempo>0){
+			var contador=setInterval(actualizarReloj(),1000);
+			//setTimeout(actualizarReloj(totalTiempo,3,2),1000);
+		}
+
+	}
+
+	function limpiar(){
+		var txtSerie=document.getElementById('serie');
+		var txtTiempo=document.getElementById('tiempo');
+		var txtDescanso=document.getElementById('descanso');
+
+		txtSerie.value="";
+		txtTiempo.value="";
+		txtDescanso.value="";
+
+	}
---
+++ sw.js
@@ -0,0 +1,80 @@
+const CACHE_NAME = 'cache-1';
+
+self.addEventListener('install', e => {
+
+
+    const cacheProm = caches.open( CACHE_NAME )
+        .then( cache => {
+
+            return cache.addAll([
+                '/',
+                'index.html',
+                'estilo.css',
+                'app.js',
+                'anthorn2.mp3',
+                'coche_8.mp3',
+                'trucks036.mp3',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+                'img/favicon.ico',
+                'img/android-icon-192x192.png',
+                'img/apple-icon-57x57.png',
+                'img/apple-icon-60x60.png',
+                'img/apple-icon-72x72.png',
+                'img/apple-icon-76x76.png',
+                'img/apple-icon-114x114.png',
+                'img/apple-icon-120x120.png',
+                'img/apple-icon-144x144.png',
+                'img/apple-icon-152x152.png',
+                'img/apple-icon-180x180.png',
+                'img/favicon-16x16.png',
+                'img/favicon-32x32.png',
+                'img/favicon-96x96.png',
+                'img/favicon-256x256.png',
+                'img/ms-icon-70x70.png',
+                'img/ms-icon-144x144.png',
+                'img/ms-icon-150x150.png',
+                'img/ms-icon-310x310.png'
+            ]);
+
+        
+        });
+
+    
+
+
+    e.waitUntil( cacheProm );
+
+});
+
+
+self.addEventListener('fetch', e =>{
+
+	  const respuesta = caches.match( e.request )
+         .then( res => {
+
+             if ( res ) return res;
+
+             // No existe el archivo
+             // tengo que ir a la web
+             console.log('No existe', e.request.url );
+
+
+             return fetch( e.request ).then( newResp => {
+
+                 caches.open( CACHE_NAME )
+                     .then( cache => {
+                         cache.put( e.request, newResp );
+                         
+                     });
+
+                 return newResp.clone();
+             });
+
+
+         });
+
+
+
+
+     e.respondWith( respuesta );
+});
